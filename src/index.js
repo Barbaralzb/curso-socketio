fa69baed1b861cd0fff44f9d56d8d1a00b17d01c
Barbara <97482282+Barbaralzb@users.noreply.github.com>
2022-10-23 18:59:48 +0200

    on, once y off

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const io = new Server(httpServer)
 
 app.use(express.static(path.join(__dirname, 'views')))
 
+const socketsOnline = []
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
@@ -19,8 +21,42 @@ app.get('/', (req, res) => {
 
 io.on("connection", socket => {
 
+    socketsOnline.push(socket.id)
+
+    // Voy a emitir un evento que llamare 'welcome' con un texto como contenido
+    socket.emit('welcome', 'Ahora estas conectado 👽')
+
+    // A aca me comunico solo con el SOCKET 
+    socket.on('server', res => {
+        console.log(res)
+    })
+
+    // Aca emito un evento a todos gracias a IO
+    io.emit('everyone', socket.id + 'se a conectado')
+
+    // Gestionar un evento al ultimo socket que se conecto
+    socket.on('last', message => {
+        const lastSocket = socketsOnline[ socketsOnline.length - 1 ];
+        io.to(lastSocket).emit('saludar', message)
+    })
+
+    // on -> podemos escuchar un evento que se emite varias veces
+    // once -> solo 1 vez
+    // off -> se usa para dejar de escuchar un evento
+
+    socket.emit('once', 'holi')
+    socket.emit('once', 'holi')
+    socket.emit('once', 'holi')
+
+    socket.emit('off', 'holi')
+    // Aca emito el evento off despues de 3 seg pero no se va a ejecutar ya que lo apagamos en el cliente
+    setTimeout(() => {
+        socket.emit('off', 'holi')
+    }, 3000)
 
 })
+
+
 
 
 httpServer.listen(3000)
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,1 +1,45 @@
 const socket = io();
+
+// Aca voy a escuchar lo que el servidor emitio, que es 'welcome', y su contenido lo imprimo en consola  
+socket.on('welcome', res => {
+    //console.log(res)
+    text.textContent = res
+})
+
+const EmitToServer = document.querySelector('#emit-to-server')
+EmitToServer.addEventListener('click', () => {
+    socket.emit('server', 'Hola servidor')
+})
+
+socket.on('everyone', res => {
+    console.log(res)
+})
+
+const EmitToLast = document.querySelector('#emit-to-last')
+EmitToLast.addEventListener('click', () => {
+    socket.emit('last', 'Hola last 🧚🏼')
+})
+
+socket.on('saludar', res => {
+    console.log(res)
+})
+
+// Aqui puedo reflejar que mi emision ONCE que el servidor emite 3 veces
+// escucha solo 1 vez cuando utilizo .once
+socket.once('once', res => {
+    console.log(res)
+})
+socket.on('once', res => {
+    console.log(res)
+})
+
+const listener = () => {
+    console.log("Se apaga el evento")
+}
+
+socket.on('off', listener)
+
+setTimeout(() => {
+    //en el metodo off tengo que tener el metodo en una variable para que funcione
+    socket.off('off', listener)
+}, 2000)
